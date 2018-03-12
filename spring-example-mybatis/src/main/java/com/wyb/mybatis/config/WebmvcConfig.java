33cf286dcbc20f4a3c9d4789e88d955a27a0bbf4
wangyingbo <wangyingbo@winupon.net>
2018-03-12 15:23:39 +0800

    集成swagger

---
+++ spring-example-mybatis/src/main/java/com/wyb/mybatis/config/WebmvcConfig.java
@@ -0,0 +1,21 @@
+package com.wyb.mybatis.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Description:
+ *
+ * @author: Kunzite
+ * @version: 2018-03-08 9:47
+ */
+@Configuration
+public class WebmvcConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+    }
+}
