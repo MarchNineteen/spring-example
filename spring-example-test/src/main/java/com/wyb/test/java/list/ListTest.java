0ea904b9c8e127988637a52305b37effb76bd0c7
Marcher <wangyb@winupon.com>
2019-06-12 19:35:09 +0800

    list test demo

---
+++ spring-example-test/src/main/java/com/wyb/test/java/list/ListTest.java
@@ -0,0 +1,22 @@
+/*
+ * @(#)ListTest    Created on 2019/3/18
+ * Copyright (c) 2019 ZDSoft Networks, Inc. All rights reserved.
+ * $$ Id$$
+ */
+package com.wyb.test.java.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wangyb
+ * @version $$ Revision: 1.0 $$, $$ Date: 2019/3/18 12:33 $$
+ */
+public class ListTest {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(2);
+    }
+}
