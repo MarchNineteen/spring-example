17755a2670e51477dfc9392b09a2b7707f19e5b1
Marcher <wangyb@winupon.com>
2019-08-26 19:03:40 +0800

    add log module

---
+++ spring-example-log/src/main/java/com/wyb/log/SpringExampleLogApplication.java
@@ -0,0 +1,13 @@
+package com.wyb.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExampleLogApplication.class, args);
+    }
+
+}
