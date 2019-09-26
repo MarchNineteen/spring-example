6b686b157378713658b206576d603541274c9bd1
Marcher <wangyb@winupon.com>
2019-09-26 19:07:21 +0800

    rm jedis

--- spring-example-cache/src/test/java/com/wyb/cache/RepositoryTests.java
+++ spring-example-cache/src/test/java/com/wyb/cache/RepositoryTests.java
@@ -1,6 +1,9 @@
 package com.wyb.cache;
 
 import com.wyb.cache.config.MemcacheConfig;
+import com.wyb.cache.constant.CacheType;
+import com.wyb.cache.factory.CacheFactory;
+import com.wyb.cache.service.CacheService;
 import net.spy.memcached.MemcachedClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,12 +18,21 @@ public class RepositoryTests {
 
 	@Resource
     private MemcacheConfig memcacheConfig;
+	@Resource
+	private CacheFactory cacheFactory;
 
 	@Test
 	public void testSetGet()  {
 		MemcachedClient memcachedClient = memcacheConfig.client();
 		memcachedClient.set("testkey",1000,"666666");
 		System.out.println("***********  "+memcachedClient.get("testkey").toString());
+	}
+
+	@Test
+	public void testRedisSet()  {
+		CacheService cacheService = cacheFactory.getCache(CacheType.REDIS);
+		cacheService.putCache("11", "11 value");
+		System.out.println(cacheService.getCache("11"));
 	}
 
 }
