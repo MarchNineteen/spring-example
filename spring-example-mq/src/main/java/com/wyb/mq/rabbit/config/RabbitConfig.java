8eff2b07ba59afda366e1d8e43467eca52c9048c
Marcher <wangyb@winupon.com>
2019-10-15 16:37:39 +0800

    rabbitmq 学习测试

--- spring-example-mq/src/main/java/com/wyb/mq/rabbit/config/RabbitConfig.java
+++ spring-example-mq/src/main/java/com/wyb/mq/rabbit/config/RabbitConfig.java
@@ -63,7 +63,7 @@ public class RabbitConfig {
         boolean durable = true;
         // 仅创建者可以使用的私有队列，断开后自动删除
         boolean exclusive = false;
-        // 当所有消费客户端连接断开后，是否自动删除队列
+        // 至少有一个消费者连接到这个队列，之后所有与这个队列连接的消费者都断开时，才会自动删除
         boolean autoDelete = false;
         return new Queue(name, durable, exclusive, autoDelete, args);
     }
--- spring-example-mq/src/main/java/com/wyb/mq/rabbit/mq/listener/SendMessageListener.java
+++ spring-example-mq/src/main/java/com/wyb/mq/rabbit/mq/listener/SendMessageListener.java
@@ -27,6 +27,29 @@ public class SendMessageListener {
 
         System.out.println(message.getMessageProperties().getDeliveryTag());
 
+//        channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);// true 重新存入队列，发给下一个订阅者 false直接移除
+        try {
+            // 参数校验
+            Assert.notNull(sendMessage, "sendMessage 消息体不能为NULL");
+//            System.out.println(1/0);
+            // TODO 处理消息
+
+            // 确认消息已经消费成功 multiple：为了减少网络流量，手动确认可以被批处理，当该参数为 true 时，则可以一次性确认 delivery_tag 小于等于传入值的所有消息
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (Exception e) {
+            logger.error("MQ消息处理异常，消息体:{}", message.getMessageProperties().getCorrelationId(), JSON.toJSONString(sendMessage), e);
+
+            // 确认消息已经消费消费失败，将消息发给下一个消费者
+            channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
+        }
+    }
+
+    @RabbitListener(queues = RabbitConstants.QUEUE_NAME_DEAD_QUEUE)
+    public void processDlx(SendMessage sendMessage, Channel channel, Message message) throws Exception {
+        logger.info("[{}]处理死信队列消息队列接收数据，消息体：{}", RabbitConstants.QUEUE_NAME_DEAD_QUEUE, JSON.toJSONString(sendMessage));
+
+        System.out.println(message.getMessageProperties().getDeliveryTag());
+
         try {
             // 参数校验
             Assert.notNull(sendMessage, "sendMessage 消息体不能为NULL");
@@ -36,7 +59,7 @@ public class SendMessageListener {
             // 确认消息已经消费成功
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
         } catch (Exception e) {
-            logger.error("MQ消息处理异常，消息体:{}", message.getMessageProperties().getCorrelationIdString(), JSON.toJSONString(sendMessage), e);
+            logger.error("MQ消息处理异常，消息体:{}", message.getMessageProperties().getCorrelationId(), JSON.toJSONString(sendMessage), e);
 
             // 确认消息已经消费消费失败，将消息发给下一个消费者
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), true);
