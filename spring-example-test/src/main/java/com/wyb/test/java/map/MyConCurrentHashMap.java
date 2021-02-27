ea9e264ca8948dbbdc9d9b1446d5e8e881cfa129
Marcher <1501987783@qq.com>
2021-02-27 13:26:49 +0800

    update

--- spring-example-test/src/main/java/com/wyb/test/java/map/MyConCurrentHashMap.java
+++ spring-example-test/src/main/java/com/wyb/test/java/map/MyConCurrentHashMap.java
@@ -1,7 +1,5 @@
 package com.wyb.test.java.map;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -10,7 +8,20 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MyConCurrentHashMap {
 
     public static void main(String[] args) {
+
         ConcurrentHashMap<String, String> chm = new ConcurrentHashMap<>();
+        chm.put("1", "1");
+
+        Thread t1 = new Thread(() -> {
+            System.out.println(chm.get("1"));
+
+        });
+        t1.start();
+
+        Thread t2 = new Thread(() -> {
+            chm.put("1", "2");
+        });
+        t2.start();
 //        chm.put();
 //        chm.size();
 //        HashMap map = new HashMap();
