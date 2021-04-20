924e45fffe64327de1eebace4b4fc5ee17b6bb0b
MarchNineteen <1501987783@qq.com>
2021-04-20 11:45:21 +0800

    add  webSocket

---
+++ spring-example-ws/src/main/java/com/wyb/ws/SpringExampleWsApplication.java
@@ -0,0 +1,16 @@
+package com.wyb.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Marcher丶
+ */
+@SpringBootApplication
+public class SpringExampleWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExampleWsApplication.class, args);
+    }
+
+}
---
+++ spring-example-ws/src/main/java/com/wyb/ws/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.wyb.ws.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author Marcher丶
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
